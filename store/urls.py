25c5a35da4308e5f282c73e86446d64680ce245d
Samuel Xia <samuelxia@samuels-air.lan>
2021-03-20 17:12:07 -0700

    first commit

---
+++ store/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('',store, name='store'),
+    path('cart/', cart, name='cart'),
+    path('checkout/', checkout, name='checkout'),
+    path('update-item/', updateItem, name='update-item'),
+    path('process-order/', processOrder, name='process-order'),
+
+]
